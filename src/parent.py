2fb983e1b3729464bcf1795135005a4968a53756
Mohamad-Hussein <mohamadhussein15@hotmail.com>
2023-11-17 22:19:54 -0700

    add huge fix, logs, global vars, logic fixes

--- src/parent.py
+++ src/parent.py
@@ -1,4 +1,7 @@
 from platform import system
+from time import time
+from os.path import join
+import logging
 
 # Differentiate between windows and linux
 if system() == 'Windows':
@@ -14,9 +17,22 @@ from multiprocessing import Process, Event, Pipe
 
 from pyaudio import PyAudio, paInt16
 
+# Global variables
+# -------------------------
+
+# Configure the logging settings
+logging.basicConfig(
+    level=logging.DEBUG,  # Set the desired logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Define the log message format
+    filename='example.log',  # Specify the log file name
+    filemode='w'  # 'w' for write, 'a' for append
+)
+# Create a logger instance
+logger = logging.getLogger(__name__)
+
 audio = PyAudio()
 
-stream = audio.open(
+stream_input = audio.open(
     format=paInt16,
     channels=1,
     rate=44100,
@@ -24,19 +40,55 @@ stream = audio.open(
     frames_per_buffer=1024,
 )
 
+stream_output = audio.open(
+    format=paInt16,
+    channels=1,
+    rate=44100,
+    output=True,
+)
+
+# Get sound data
+file_low = open(join('effects', 'button-low.wav'), 'rb')
+file_high = open(join('effects', 'button-high.wav'), 'rb')
+sound_low = file_low.readframes(file_low.getnframes())
+sound_high = file_high.readframes(file_high.getnframes())
+file_low.close()
+file_high.close()
+# -------------------------
+
 def start_audio(start_event):
-    # global audio, stream
+    # This line to wake device from sleep state
+    stream_output.write(sound_high)
+
+    # stream_input.start_stream()
+    logger.debug(f"stream is stopped: {stream_input.is_stopped()}")
+    logger.debug(f"Get read: {stream_input.get_read_available()}")
+    logger.debug(f"Is active: {stream_input.is_active()}")
+
+    if not stream_input.is_active():
+        print("Stream is not active")
+        return
+    
+
     frames = []
 
     print("Started audio recording")
+
     try:
         while start_event.is_set():
             print("Capture")
-            data = stream.read(1024)
+            data = stream_input.read(1024)
             frames.append(data)
+
+        stream_output.write(sound_low)
+        print("Capture FINISHED")
+    
     except KeyboardInterrupt:
         print("Keyboard interrupt")
-        pass
+        return
+    except Exception:
+        print(f"\nCAPTURE UNSUCCESFUL!")
+        return
     
     # Sound file creation
     sound_file = open("recording.wav", "wb")
@@ -53,43 +105,46 @@ def run_listener(child_pipe, start_event):
 
 
 def main():
+    # Input device
+    print(f"This is the input device used: {audio.get_default_input_device_info()['name']}")
     # Creating pipes just in case
     parent_pipe, child_pipe = Pipe(duplex=False)
 
     # Events for synchronization
     start_event = Event()
-    stop_event = Event()
     model_event = Event()
 
-    # Creating processes (model first)
-    model_process = Process(target=service, args=(child_pipe, model_event))
+    # Creating processe for model as it takes the longest to load
+    model_process = Process(target=service, args=(child_pipe, model_event), name='WhisperModel')
     model_process.start()
 
-    userinput_process = Process(target=run_listener, args=(child_pipe, start_event))
+    # Waiting for model to load
+    print(f"Waiting for model to load")
+    model_event.wait()
+    model_event.clear()
+    print(f"Model loaded!\n\n")
+
+    # Creating process for Key listener
+    userinput_process = Process(target=run_listener, args=(child_pipe, start_event), name='SA-KeyListener')
     userinput_process.start()
-    
+
     try:
         while 1:
             try:
                 # Waiting for Start event
-                print("Waiting now")
+                print("Waiting for hotkey")
                 start_event.wait()
 
-                # Start recording process
-                print("Recording")
-
+                # Previously had seperate process running, however 3 concurrent runnnig is enough
                 # recording_process = Process(target=start_audio, args=(stop_event, audio, stream))
                 # recording_process.start()
-                start_audio(start_event)
-                # Waiting for Stop
-                print("Waiting for stop")
-                while start_event.is_set():
-                    pass
-                
-                # Stopping recording
-                stop_event.set()
 
-                # see a way to stop it
+                # Starting to Record
+                print("Recording")
+                if start_event.is_set():
+                    start_audio(start_event)
+                else:
+                    print("Did not record properly")
 
                 # Inference
                 print("Starting inference")
@@ -98,18 +153,18 @@ def main():
                     pass
 
                 # Clearing events
-                stop_event.clear()
                 start_event.clear()
 
-                print("Finished inference")
-
             except Exception as e:
+                print(f"Exception on parent!\n\n")
                 print(e)
                 break
 
     # To ensure that the processes are closed
     finally:
         userinput_process.join()
-        stream.stop_stream()
-        stream.close()
+        stream_input.stop_stream()
+        stream_output.stop_stream()
+        stream_input.close()
+        stream_output.close()
         audio.terminate()
