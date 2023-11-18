90a52865e4ae0fdf9f748a9f9f6afeaf5f38cb63
Mohamad-Hussein <mohamadhussein15@hotmail.com>
2023-11-18 11:05:18 -0700

    add printing device name

--- src/model_inference.py
+++ src/model_inference.py
@@ -1,11 +1,15 @@
 from time import sleep, time
-from torch.cuda import is_available
+from torch.cuda import is_available, get_device_name
 from torch import float16, float32
 from pyautogui import typewrite
 from os.path import join
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+import logging
 
-MODEL_ID = "distil-whisper/distil-large-v2"  # ~1700-2000 MiB of GPU memory
+MODEL_ID = "distil-whisper/distil-medium.en"  # ~900-1500 MiB of GPU memory
+# MODEL_ID = "distil-whisper/distil-large-v2"  # ~1700-2000 MiB of GPU memory
+
+logger = logging.getLogger(__name__)
 
 
 def service(pipe, event):
@@ -38,6 +42,7 @@ def service(pipe, event):
     )
 
     # Clearing memory
+    print(f"\nModel loaded to {get_device_name(device)}\n\n")
     del device, torch_dtype, local_cache_dir, processor
 
     # Telling parent that model is loaded
@@ -46,17 +51,20 @@ def service(pipe, event):
     sleep(1)
 
     # Waits in standy for inference
-    file_path = "recording.wav"
+    file_path = "tmp.wav"
 
     # Make sure event is cleared before then
     try:
         while 1:
             event.wait()
             t0 = time()
-            print("Inference event set")
             result = pipe(file_path)
-            print(f"\nPrinted text: {result['text']}\nTime for inference: {time() - t0}")
+            logger.info(f"Time for inference: {time() - t0:.4f} seconds")
             typewrite(result["text"])
+            print(
+                f"\nPrinted text: {result['text']}\nSpeech-to-text time: {time() - t0:.3f}s\n"
+            )
+            logger.debug(f"Result: {result}")
             event.clear()
 
     finally:
