49f71239ff1bd2b982c3fbfa482b91a5a6f87e07
Mohamad-Hussein <mohamadhussein15@hotmail.com>
2023-11-18 00:09:35 -0700

    add reliable hotkey capture

--- src/key_listener_win.py
+++ src/key_listener_win.py
@@ -1,4 +1,4 @@
-from keyboard import add_hotkey, unhook_all, on_release_key
+from keyboard import add_hotkey, unhook_all, on_release_key, wait, is_pressed
 from time import sleep
 
 class Listener:
@@ -8,7 +8,7 @@ class Listener:
         self.hotkey_held = False
 
         # -- Hotkey --
-        self.hotkey = "left windows", "shift"
+        self.hotkey = "left windows + shift"
         # ------------
         
 
@@ -19,23 +19,28 @@ class Listener:
             self.start_event.set()
             self.hotkey_held = True
 
-    def up(self, e):
-        print(f"Key {e.name}")
+    def up(self, e=None):
+        # print(f"Key {e.name}")
         if self.hotkey_held and self.start_event.is_set():
-            print(f"{self.hotkey} is released")
+            # print(f"{self.hotkey} is released")
             self.start_event.clear()
             self.hotkey_held = False
 
 
     def run(self):
-        print(f"{' + '.join(self.hotkey)}")
-        add_hotkey(' + '.join(self.hotkey), self.down)
-        on_release_key(self.hotkey, self.up)
+        print(f"Hotkey assigned: {self.hotkey}")
+        # add_hotkey(' + '.join(self.hotkey), self.down)
+        # on_release_key(self.hotkey, self.up)
 
         try:
             while 1:
-                print(self.start_event)     
-                sleep(1)
+                wait(self.hotkey)
+                self.down()
+                # Wait until it is not pressed anymore
+                while is_pressed(self.hotkey):
+                    pass
+                self.up()
+
         finally:
             print("Ending hotkey listener")
             unhook_all()
