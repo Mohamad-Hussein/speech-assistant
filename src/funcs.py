6ecb37ded04dbc5ce15c0ba223bda0d95c9a363e
Mohamad-Hussein <mohamadhussein15@hotmail.com>
2023-11-18 16:18:02 -0700

    rem unused function calls

--- src/funcs.py
+++ src/funcs.py
@@ -1,6 +1,4 @@
 from pyaudio import PyAudio, paInt16
-from wave import open
-from os.path import join
 import logging
 from platform import system
 
@@ -18,23 +16,7 @@ def get_audio():
         frames_per_buffer=1024,
     )
 
-    stream_output = audio.open(
-        format=paInt16,
-        channels=1,
-        rate=44100,
-        output=True,
-    )
-    return audio, stream_input, stream_output
-
-
-def get_effects(dir_name: str, sound_low_name: str, sound_high_name: str):
-    file_low = open(join(dir_name, sound_low_name), "rb")
-    file_high = open(join(dir_name, sound_high_name), "rb")
-    sound_low = file_low.readframes(file_low.getnframes())
-    sound_high = file_high.readframes(file_high.getnframes())
-    file_low.close()
-    file_high.close()
-    return sound_low, sound_high
+    return audio, stream_input
 
 
 def run_listener(child_pipe, start_event):
