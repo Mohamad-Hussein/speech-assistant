875e2bb90dcdbd53f3a152eb655b1e1f1700615e
Mohamad-Hussein <mohamadhussein15@hotmail.com>
2023-12-19 16:44:13 -0700

    fix extra steps of manual gpu detection

--- src/funcs.py
+++ src/funcs.py
@@ -154,34 +154,34 @@ def find_gpu_config(logger):
         logger.info(f"Device capability: {cuda.get_device_capability()}")
         logger.info(f"Current memory allocated: {cuda.mem_get_info()}")
 
-    # AMD, change 0 to 1 if you have an AMD GPU
-    elif 0:
+    # AMD
+    else:
         try:
             import torch_directml as dml
-        except ImportError:
-            print(
-                f"Please install torch_directml to your environment to check if you have AMD GPU,\n"
-                + "Use ` pip install torch-directml `."
-                "If you don't and want to use CPU, although not recommended, "
-                + "comment out this else statement."
-            )
-            logger.error(f"Package directml not found")
-            exit(1)
+            
+            if dml.is_available():
+                torch_dtype = float16
+                device = dml.device()
+                device_name = dml.device_name(dml.default_device())
 
-        if dml.is_available():
-            torch_dtype = float16
-            device = dml.device()
-            device_name = dml.device_name(dml.default_device())
+                logger.debug("GPU detected from torch_directml")
+                logger.info(f"Available: {dml.is_available()}")
+                logger.info(f"Devices Available: {dml.device_count()}")
+                logger.info(f"Device: {device}")
+                logger.info(f"Default device: {dml.default_device()}")
+                logger.info(f"Device name: {dml.device_name(0)}")
+                logger.info(f"GPU memory: {dml.gpu_memory()}")
+            else:
+                torch_dtype = float32
 
-            logger.debug("GPU detected from torch_directml")
-            logger.info(f"Available: {dml.is_available()}")
-            logger.info(f"Devices Available: {dml.device_count()}")
-            logger.info(f"Device: {device}")
-            logger.info(f"Default device: {dml.default_device()}")
-            logger.info(f"Device name: {dml.device_name(0)}")
-            logger.info(f"GPU memory: {dml.gpu_memory()}")
+                logger.debug("No GPU detected, using cpu")
+                logger.warning(
+                    "Attention, using the CPU is not recommended! Computation time will be long."
+                )
 
-        else:
+        # Use CPU if directml is not installed
+        except Exception:
+            logger.debug(f"Package directml not found")
             torch_dtype = float32
 
             logger.debug("No GPU detected, using cpu")
@@ -189,7 +189,7 @@ def find_gpu_config(logger):
                 "Attention, using the CPU is not recommended! Computation time will be long."
             )
 
-    logger.debug(
+    logger.info(
         f"GPU config -- device: {device}, device name: {device_name}, torch_dtype: {torch_dtype}"
     )
     return device, device_name, torch_dtype
