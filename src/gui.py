40b26b1c4c4a63ef034abe2fecbaeb75e6b3b6db
Mohamad-Hussein <mohamadhussein15@hotmail.com>
2024-02-04 21:58:25 -0500

    add start and stop to SA

--- src/gui.py
+++ src/gui.py
@@ -3,8 +3,12 @@ from tkinter import Tk, Button, DISABLED, NORMAL
 from threading import Thread
 
 from src.parent import main_loop
-from src.funcs import run_listener
+from src.funcs import run_listener, type_writing, copy_writing
 from src.model_inference import service
+
+
+# Choosing which way to write text.
+WRITE = type_writing
 
 
 class SpeechDetectionGUI:
@@ -14,14 +18,19 @@ class SpeechDetectionGUI:
         self.model_process = None
         self.parent_process = None
 
-        # Concurrency variables
-        self.start_event = None
-        self.model_event = None
-        self.terminate_event = None
-        self.sound_data_queue = None
-        self.parent_pipe = None
-        self.child_pipe = None
+        ## Concurrency variables
+        # Events for synchronization
+        self.start_event = Event()
+        self.model_event = Event()
+        self.terminate_event = Event()
 
+        # Queue for audio data
+        self.sound_data_queue = Queue()
+
+        # Pipes for communication (not used yet)
+        self.parent_pipe, self.child_pipe = Pipe()
+
+        ## GUI ##
         # GUI parameters
         self.root = Tk()
         self.root.title("Speech-Assistant")
@@ -45,31 +54,12 @@ class SpeechDetectionGUI:
         # GUI protocols
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
 
-    def init_system(self):
-        """
-        Initializes the system by creating the necessary concurrency constructs
-        and starting the processes.
-        """
-
-        # Creating pipes for sending audio bytes
-        self.parent_pipe, self.child_pipe = Pipe()
-        # model_recv_pipe, model_send_pipe = Pipe() # duplex=True is faster than False
-
-        # Events for synchronization
-        self.start_event = Event()
-        self.model_event = Event()
-        self.terminate_event = Event()
-
-        # Slower than Pipe however it could handle more data
-        self.sound_data_queue = Queue()
-
-        # Creating process for model as it takes the longest to load
+    def load_model(self):
+        """Loads the ASR model and starts the model service."""
+        ## Creating process for model as it takes the longest to load
         self.model_process = Process(
             target=service,
-            args=(
-                self.sound_data_queue,
-                self.model_event,
-            ),
+            args=(self.sound_data_queue, self.model_event, WRITE),
             name="WhisperModel",
         )
         self.model_process.start()
@@ -79,7 +69,24 @@ class SpeechDetectionGUI:
         self.model_event.wait()
         self.model_event.clear()
 
-        # Creating process for Key listener
+    def init_system(self):
+        """
+        Initializes the system by creating the necessary
+        concurrency constructs and starting the processes.
+        """
+
+        # Clearing events
+        self.model_event.clear()
+        self.start_event.clear()
+        self.terminate_event.clear()
+
+        ## Loading model depending on the case
+        if not self.model_process:
+            self.load_model()
+        else:
+            self.sound_data_queue.put("Load model")
+
+        ## Creating process for Key listener
         self.key_listener_thread = Thread(
             target=run_listener,
             args=(
@@ -96,15 +103,7 @@ class SpeechDetectionGUI:
         self.start_event.wait()
         self.start_event.clear()
 
-    def start_detection(self):
-        """Starts the speech detection process"""
-        # Button state change
-        self.start_button.config(state=DISABLED)
-
-        # Initializing system
-        self.init_system()
-
-        # Creating process for parent
+        ## Creating process for parent
         self.parent_process = Process(
             target=main_loop,
             args=(
@@ -117,34 +116,60 @@ class SpeechDetectionGUI:
         )
         self.parent_process.start()
 
+    def start_detection(self):
+        """Starts the speech detection process"""
+        # Button state change
+        self.start_button.config(state=DISABLED)
+
+        # Initializing system
+        self.init_system()
+
         # Button state change
         self.stop_button.config(state=NORMAL)
 
     def stop_detection(self):
-        # For model process to terminate
+        """
+        Stops the speech detection process by
+        telling the model to unload and terminating
+        parent and key listener processes.
+        """
+        # For model to unload from memory
         self.sound_data_queue.put(None)
 
         # For parent process to terminate
         self.terminate_event.set()
         self.start_event.set()
 
+        # Making sure processes are joined
         self.parent_process.join()
-        self.model_process.join()
+        self.key_listener_thread.join()
 
         # Button state change
         self.start_button.config(state=NORMAL)
         self.stop_button.config(state=DISABLED)
 
     def on_close(self):
-        # Terminates all processes before closing
-        if self.stop_button["state"] == NORMAL:
-            self.stop_detection()
+        """Terminates all processes before closing"""
+
+        # To tell model process to terminate
+        self.sound_data_queue.put("Terminate")
+
+        # For parent process to terminate
+        self.terminate_event.set()
+        self.start_event.set()
+
+        # Terminate processes and joining threads
+        if self.model_process:
+            self.model_process.terminate()
+            self.model_process.join()
+        if self.parent_process:
+            self.parent_process.terminate()
+            self.parent_process.join()
+        if self.key_listener_thread:
+            self.key_listener_thread.join()
 
         # Destroys the GUI
         self.root.destroy()
 
     def run(self):
         self.root.mainloop()
-        self.parent_process.join()
-        self.model_process.join()
-        self.key_listener_thread.join()
