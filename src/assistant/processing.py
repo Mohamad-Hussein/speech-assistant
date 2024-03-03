e06432a16bcee6c9d89aa15c830499112ff25ed0
Mohamad-Hussein <mohamadhussein15@hotmail.com>
2024-03-02 19:16:03 -0500

    add force to copy_write when non latin characters appear

--- src/assistant/processing.py
+++ src/assistant/processing.py
@@ -1,3 +1,28 @@
+from src.utils.funcs import copy_writing
+
+
+def perform_request(text: str, start_event, prev_text, write_method):
+    """Performs the request of the user based on action
+
+    Args:
+        text (str): The input text for the request
+        start_event (Event): The event that triggered the request
+        prev_text (str): The previous text in the conversation, if applicable. Defaults to None.
+        write_method (Callable): A function to call with the output text, if necessary. Defaults to None.
+
+    Returns:
+        str: The processed text
+    """
+    text = process_text(text, start_event, prev_text)
+
+    # If the text is not English, then copy it
+    if notAscii(text):
+        write_method = copy_writing
+
+    write_method(text)
+
+    return text
+
 
 def process_text(text: str, start_event, prev_text) -> str:
     """
@@ -6,7 +31,7 @@ def process_text(text: str, start_event, prev_text) -> str:
 
     Args:
         text (str): The text to be processed
-        start_event (multiprocessing.Event): Event to tell the child process 
+        start_event (multiprocessing.Event): Event to tell the child process
             that the model is loaded
         prev_text (str): The previous text
 
@@ -27,3 +52,7 @@ def process_text(text: str, start_event, prev_text) -> str:
             processed = text.strip()
 
     return processed
+
+
+def notAscii(s):
+    return not s.isascii()
